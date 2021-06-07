300021c1ddb6c5ea6c06f4bda1b1d17e415019a8
IgnatYaskevich <78702174+IgnatYaskevich@users.noreply.github.com>
2021-06-07 15:50:04 +0300

    Updates

---
+++ static/js/8.ad93b44c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[8],{318:function(t,n,p){"use strict";p.r(n);var e=p(1);p(0);n.default=function(){return Object(e.jsx)("div",{children:"Setting"})}}}]);
+//# sourceMappingURL=8.ad93b44c.chunk.js.map
